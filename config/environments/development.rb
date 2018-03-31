9188eae53a0590221544877d998656bbc9f19e22
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-03-31 18:59:27 -0400

    add from mailer to development config
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 5000 }
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
