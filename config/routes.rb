00b78402d5abe20df0484a176733a9f0928b050a
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-03-31 18:41:48 -0400

    remove edit and cancel registration devise routes
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+
+  devise_for :users, skip: :registrations
+  devise_scope :user do
+    resource :registration,
+      only: [:new, :create],
+      path: 'users',
+      path_names: { new: 'sign_up' },
+      controller: 'devise/registrations',
+      as: :user_registration do
+        # get :cancel
+      end
+  end
 end
