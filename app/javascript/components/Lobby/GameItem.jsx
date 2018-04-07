a92a13a61364087f3ac6d95e3438e16c5eee40ee
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-04-07 16:39:50 -0400

    additional proptype and eslint fixes
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/javascript/components/Lobby/GameItem.jsx
+++ app/javascript/components/Lobby/GameItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class GameItem extends React.Component {
   render() {
@@ -9,10 +10,14 @@ class GameItem extends React.Component {
         <div className="game-item host">{host_id}</div>
         <div className="game-item created">{created_at}</div>
         <div className="game-item players">{players}</div>
-        <div className="game-item join">join</div>
+        <div className="game-item join">Join This Game</div>
       </div>
     );
   }
 }
+
+GameItem.propTypes = {
+  game: PropTypes.object.isRequired,
+};
 
 export default GameItem;
