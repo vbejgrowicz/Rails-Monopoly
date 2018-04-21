cefaf72f606ad2f4304e80e82819c890ace6a0bc
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-04-21 11:43:00 -0400

    renderTitle function to handle corners
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/javascript/components/Board/GameTile.jsx
+++ app/javascript/components/Board/GameTile.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const cornerTitle = {
+  'go-to-jail': item => (
+    <div className={`title ${item.category}`}>
+      <span>go to</span><span>jail</span>
+    </div>
+  ),
+  'free-parking': item => (
+    <div className={`title ${item.category}`}>
+      <span>free</span><span>parking</span>
+    </div>
+  ),
+  'go': item => (<div className={`title ${item.category}`}>{item.description}</div>),
+  'in-jail': item => (
+    <div className={`title ${item.category}`}>
+      <span>just</span><span>visiting</span>
+    </div>
+  ),
+};
+
 class GameTile extends React.Component {
+  constructor() {
+    super();
+
+    this.renderTitle = this.renderTitle.bind(this);
+  }
+
+  renderTitle() {
+    const { item } = this.props;
+    if (cornerTitle[item.category]) {
+      return cornerTitle[item.category](item);
+    }
+    return (
+      <div className={`title ${item.category}`}>{item.title}</div>
+    );
+  }
   render() {
     const { item } = this.props;
     return (
       <div className={item.position % 10 === 0 ? 'corner' : 'tile'}>
         <div className="tile-data">
           <div className={item.color && `color ${item.color}`} />
-          <div className="title">{item.title}</div>
+          {this.renderTitle()}
           <div className={`image ${item.category}`} />
           {item.color && (<div className="price">Price $100</div>)}
         </div>
