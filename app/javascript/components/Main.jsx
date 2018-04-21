1e5990034e551ebe89ab425a64270ec6bf96be75
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-04-21 18:19:00 -0400

    add routes for lobby and game board
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/javascript/components/Main.jsx
+++ app/javascript/components/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import GameTables from './Lobby/GameTables';
 import GameBoard from './Board/GameBoard';
@@ -7,17 +8,28 @@ import ServerError from './ServerError';
 import { fetchUserRequest, fetchUserReceived } from '../actions';
 import { get, apiRequest } from '../utils/fetch';
 
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/lobby" component={GameTables} />
+        <Route exact path="/game/:id" component={GameBoard} />
+        <Redirect from="/" to="/lobby" />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
 class Main extends React.Component {
   componentWillMount() {
     this.props.fetchUser();
   }
 
   render() {
-    return <GameBoard />;
     return this.props.isFetching === false && (
       [
         <ServerError key="1" />,
-        <GameTables key="2" />,
+        <Router key="2" />,
       ]
     );
   }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: 'home#index'
 
   devise_for :users, skip: :registrations
   devise_scope :user do
@@ -23,4 +23,6 @@ Rails.application.routes.draw do
       resources :spaces, only: [:index]
     end
   end
+
+  get '*path', to: 'home#index'
 end
