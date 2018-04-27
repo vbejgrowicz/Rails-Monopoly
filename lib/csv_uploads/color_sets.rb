c430cc4031e3334477b5b7cad2b34898dad06efe
ljr5102 <ljr5102@gmail.com>
2018-04-27 16:20:50 -0400

    pull stuff from color_sets upload and have it inherit from CsvUploader

--- lib/csv_uploads/color_sets.rb
+++ lib/csv_uploads/color_sets.rb
@@ -1,19 +1,11 @@
-class CsvUploads::ColorSets
-  def self.run
-    puts '--Uploading ColorSets--'
-    CSV.foreach("#{Rails.root}/db/csv_seeds/color_sets.csv", headers: true) do |row|
-      begin
-        color_set = ColorSet.find_or_initialize_by(id: row['id'].to_i)
-        color_set.attributes = row.to_hash
-        if color_set.changed?
-          color_set.save!
-          puts "#{color_set.color} created!"
-        else
-          puts "#{color_set.color} unchanged"
-        end
-      rescue StandardError => e
-        puts e.message
-      end
+module CsvUploads
+  class ColorSets < CsvUploader
+    def self.identifier
+      "ColorSet"
+    end
+
+    def self.label
+      :color
     end
   end
 end
