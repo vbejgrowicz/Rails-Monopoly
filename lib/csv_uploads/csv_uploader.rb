0b7f81f8fb9b2ba76782042e1cdf79cef4f7790f
ljr5102 <ljr5102@gmail.com>
2018-04-27 16:20:20 -0400

    create a parent csv_uploader class that will handle the uploads for all classes that follow a similar pattern

---
+++ lib/csv_uploads/csv_uploader.rb
@@ -0,0 +1,29 @@
+module CsvUploads
+  class CsvUploader
+    def self.identifier
+      "CsvUploader"
+    end
+
+    def self.label
+      :body
+    end
+
+    def self.run
+      puts "--Uploading #{identifier.pluralize}--"
+      CSV.foreach("#{Rails.root}/db/csv_seeds/#{identifier.pluralize.underscore}.csv", headers: true) do |row|
+        begin
+          instance = identifier.classify.constantize.find_or_initialize_by(id: row['id'].to_i)
+          instance.attributes = row.to_hash
+          if instance.changed?
+            instance.save!
+            puts "#{instance.send(label)} created!"
+          else
+            puts "#{instance.send(label)} unchanged"
+          end
+        rescue StandardError => e
+          puts e.message
+        end
+      end
+    end
+  end
+end
