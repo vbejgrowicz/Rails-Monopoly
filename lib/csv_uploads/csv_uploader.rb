ba249a20dd7bfb8251fd94e4099c623680d56188
ljr5102 <ljr5102@gmail.com>
2018-04-27 16:38:59 -0400

    have csv_uploader destroy records that aren't in the csv files

--- lib/csv_uploads/csv_uploader.rb
+++ lib/csv_uploads/csv_uploader.rb
@@ -10,6 +10,7 @@ module CsvUploads
 
     def self.run
       puts "--Uploading #{identifier.pluralize}--"
+      seed_ids = []
       CSV.foreach("#{Rails.root}/db/csv_seeds/#{identifier.pluralize.underscore}.csv", headers: true) do |row|
         begin
           instance = identifier.classify.constantize.find_or_initialize_by(id: row['id'].to_i)
@@ -24,6 +25,11 @@ module CsvUploads
         rescue StandardError => e
           puts e.message
         end
+        seed_ids << instance.id
+      end
+      identifier.classify.constantize.where.not(id: seed_ids).each do |record|
+        puts "destroying #{record.send(label)}"
+        record.destroy!
       end
     end
   end
