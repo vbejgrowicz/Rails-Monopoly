437111b78129c2df8316d31036bc98faa67d3ed8
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-04-28 12:23:40 -0400

    add token_id to player on create
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/controllers/api/players_controller.rb
+++ app/controllers/api/players_controller.rb
@@ -1,6 +1,6 @@
 class Api::PlayersController < ApplicationController
   def create
-    @player = Player.create!(game_id: params[:game_id], user_id: current_user.id)
+    @player = Player.create!(game_id: params[:game_id], token_id: params[:token_id], user_id: current_user.id)
     render json: { player: PlayerPresenter.new(@player) }
   end
 end
