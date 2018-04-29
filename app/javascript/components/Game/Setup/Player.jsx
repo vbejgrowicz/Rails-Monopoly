cf986f7cf1e0f21182e2bc2dd0d01b2e093c7880
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-04-28 22:35:36 -0400

    add roll function and dice mapper
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/javascript/components/Game/Setup/Player.jsx
+++ app/javascript/components/Game/Setup/Player.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createRollRequest, createRollReceived } from '../../../actions';
+import { post, apiRequest } from '../../../utils/fetch';
+
+const dieMapper = {
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+};
 
 class Player extends React.Component {
   constructor() {
     super();
 
     this.actionRender = this.actionRender.bind(this);
+    this.diceRender = this.diceRender.bind(this);
   }
 
   actionRender() {
     const { currentUserId } = this.props;
-    const { user_id } = this.props.player;
+    const { id, user_id } = this.props.player;
     return currentUserId === user_id ? (
-      <button className="action button">Roll</button>
+      <button className="action button" onClick={() => this.props.roll(id)}>Roll</button>
     ) : (
       <div className="action pending">Awaiting Roll...</div>
     );
   }
 
   diceRender() {
+    const { die_one, die_two } = this.props.player.roll;
     return (
       <div className="dice-group">
-        <div className={`die one`} />
-        <div className={`die two`} />
+        <div className={`die ${dieMapper[die_one]}`} />
+        <div className={`die ${dieMapper[die_two]}`} />
       </div>
     );
   }
@@ -35,7 +49,7 @@ class Player extends React.Component {
         </div>
         {gameStarted && (
           <div className="roll-group">
-            {player.roll ? this.diceRender() : this.actionRender()}
+            {player.roll.id ? this.diceRender() : this.actionRender()}
           </div>
         )}
       </div>
@@ -47,6 +61,17 @@ Player.propTypes = {
   currentUserId: PropTypes.number.isRequired,
   player: PropTypes.object.isRequired,
   gameStarted: PropTypes.bool.isRequired,
+  roll: PropTypes.func.isRequired,
 };
 
-export default Player;
+const mapDispatchToProps = dispatch => ({
+  roll: async (playerId) => {
+    dispatch(createRollRequest());
+    const createRoll = () => post(`/api/players/${playerId}/rolls`);
+    apiRequest(createRoll, (json) => {
+      dispatch(createRollReceived(json.roll));
+    });
+  },
+});
+
+export default connect(null, mapDispatchToProps)(Player);
