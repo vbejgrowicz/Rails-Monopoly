89cfdcdbe482f0b068c3891fd89cea108ccf236d
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-04-29 16:23:26 -0400

    fix for devise to allow username to be sent in signup
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   include Errors::ErrorHandler
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
