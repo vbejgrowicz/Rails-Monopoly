d09830d4191c7b983c1384aa312cc270245edf21
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-04-29 16:23:04 -0400

    add username to users with validations
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username, presence: true, uniqueness: true
+
   has_many :players, dependent: :destroy
   has_many :hosted_games, class_name: 'Game', primary_key: :id, foreign_key: :host_id, dependent: :destroy
   has_many :active_games, through: :players, source: :game
