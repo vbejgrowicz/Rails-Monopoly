c56acff70ff5f9ce65c034a8b08f267e2ccee61e
ljr5102 <ljr5102@gmail.com>
2018-04-30 15:46:23 -0400

    update chance cards to have three descriptions

--- db/migrate/20180430191442_add_chance_cards_table.rb
+++ db/migrate/20180430191442_add_chance_cards_table.rb
@@ -1,7 +1,9 @@
 class AddChanceCardsTable < ActiveRecord::Migration[5.1]
   def change
     create_table :chance_cards do |t|
-      t.string :description, null: false
+      t.string :description_one, null: false, default: ''
+      t.string :description_two, null: false, default: ''
+      t.string :description_three, null: false, default: ''
       t.timestamps
     end
   end
