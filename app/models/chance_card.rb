13967d75bebd46d435eca0e938cdb25e0a47e55c
ljr5102 <ljr5102@gmail.com>
2018-04-30 18:07:18 -0400

    pull the shared logic between chance card and community chest card out into a concern

--- app/models/chance_card.rb
+++ app/models/chance_card.rb
@@ -1,19 +1,3 @@
 class ChanceCard < ApplicationRecord
-  validates :description, presence: true
-
-  has_many :chance_games
-  has_many :games, through: :chance_games
-
-  def self.shuffle_and_save_for_game!(game_id)
-    all.shuffle.each do |card|
-      ChanceGame.create!(chance_card_id: card.id, game_id: game_id)
-    end
-  end
-
-  def self.get_top_card_for_game(game_id)
-    joins(:chance_games)
-      .where('chance_games.game_id = ?', game_id)
-      .order('chance_games.times_used asc, chance_games.id asc')
-      .first
-  end
+  include CardActionable
 end
--- app/models/community_chest_card.rb
+++ app/models/community_chest_card.rb
@@ -1,6 +1,3 @@
 class CommunityChestCard < ApplicationRecord
-  validates :description, presence: true
-
-  has_many :community_chest_games
-  has_many :games, through: :community_chest_games
+  include CardActionable
 end
