9656cce2c8766b59836b03fc12eda4f3e27eee2e
ljr5102 <ljr5102@gmail.com>
2018-04-30 16:24:04 -0400

    method that shuffles chance cards and saves chance game record for that order

--- app/models/chance_card.rb
+++ app/models/chance_card.rb
@@ -3,4 +3,10 @@ class ChanceCard < ApplicationRecord
 
   has_many :chance_games
   has_many :games, through: :chance_games
+
+  def self.shuffle_and_save_for_game!(game_id)
+    all.shuffle.each do |card|
+      ChanceGame.create!(chance_card_id: card.id, game_id: game_id)
+    end
+  end
 end
