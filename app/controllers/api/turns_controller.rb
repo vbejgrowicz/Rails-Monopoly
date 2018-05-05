11577c447e59b7bf104302900f967aae5cb9ad83
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-05-05 14:52:50 -0400

    render turn presenter in turns_controller
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/controllers/api/turns_controller.rb
+++ app/controllers/api/turns_controller.rb
@@ -1,6 +1,6 @@
 class Api::TurnsController < ApplicationController
   def index
     @turns = Turn.where(game_id: params[:game_id]).order(id: :asc).limit(10)
-    render json: { turns: @turns.map { |turn| turn.slice(:id, :game_id, :player_id) } }
+    render json: { turns: @turns.map { |turn| TurnPresenter.new(turn) } }
   end
 end
