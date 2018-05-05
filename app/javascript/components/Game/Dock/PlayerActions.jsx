b094b4b6be3494196592b3a35e4fc9bea805f027
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-05-05 18:44:59 -0400

    roll dice on button click and update turn
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/javascript/components/Game/Dock/PlayerActions.jsx
+++ app/javascript/components/Game/Dock/PlayerActions.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { dieMapper } from '../../../utils/helpers';
+import { put, apiRequest } from '../../../utils/fetch';
+import { updateTurnRequest, updateTurnReceived } from '../../../actions';
 
 class PlayerActions extends React.Component {
   render() {
@@ -13,7 +16,11 @@ class PlayerActions extends React.Component {
           <div className={`die ${roll.id ? dieMapper[roll.die_one] : 'one'}`} />
           <div className={`die ${roll.id ? dieMapper[roll.die_two] : 'one'}`} />
         </div>
-        <button className="roll">Roll</button>
+        {roll.id ? (
+          <button className="action-btn move" onClick={() => {}}>Move Token</button>
+        ) : (
+          <button className="action-btn roll" onClick={() => this.props.updateTurn('roll', currentTurn.id)}>Roll</button>
+        )}
         <div>It's Your Turn!</div>
       </div>
     );
@@ -23,6 +30,7 @@ class PlayerActions extends React.Component {
 PlayerActions.propTypes = {
   isActivePlayer: PropTypes.bool.isRequired,
   currentTurn: PropTypes.object.isRequired,
+  updateTurn: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = ({ currentUser, turns }) => ({
@@ -30,4 +38,15 @@ const mapStateToProps = ({ currentUser, turns }) => ({
   currentTurn: turns.items[0],
 });
 
-export default connect(mapStateToProps, null)(PlayerActions);
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  updateTurn: async (turn_action, turnId) => {
+    const gameId = ownProps.match.params.id;
+    dispatch(updateTurnRequest());
+    const updateTurn = () => put(`/api/games/${gameId}/turns/${turnId}`, { turn_action });
+    return apiRequest(updateTurn, (json) => {
+      dispatch(updateTurnReceived(json.turn));
+    });
+  },
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(PlayerActions));
