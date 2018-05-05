d36c3188f768d817be44139f008effcb1dd8836d
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-05-05 14:55:25 -0400

    add active class to player card
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/javascript/components/Game/Dock/PlayerCard.jsx
+++ app/javascript/components/Game/Dock/PlayerCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class PlayerCard extends React.Component {
   render() {
-    const { token, username } = this.props.player;
+    const { token, username, id } = this.props.player;
+    const { activePlayerId } = this.props;
     return (
-      <div className="player-card">
+      <div className={`player-card ${activePlayerId === id ? 'active' : ''}`}>
         <div className={`token ${token}`} />
         <div className="player-name">{username}</div>
         <div className="player-money">$1500</div>
@@ -16,6 +18,11 @@ class PlayerCard extends React.Component {
 
 PlayerCard.propTypes = {
   player: PropTypes.object.isRequired,
+  activePlayerId: PropTypes.number.isRequired,
 };
 
-export default PlayerCard;
+const mapStateToProps = ({ turns }) => ({
+  activePlayerId: turns.items[0].player.id,
+});
+
+export default connect(mapStateToProps, null)(PlayerCard);
