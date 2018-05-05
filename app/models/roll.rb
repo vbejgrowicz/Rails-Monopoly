6380059559bc1a7726c4894169048c57d5d09444
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-05-05 19:09:18 -0400

    add total method for rolls
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -3,4 +3,8 @@ class Roll < ApplicationRecord
   validates :die_one, :die_two, numericality: { greater_than: 0, less_than: 7 }
 
   belongs_to :player
+
+  def total
+    die_one + die_two
+  end
 end
