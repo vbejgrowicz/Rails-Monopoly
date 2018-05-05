2aee85d4d10d55096a34a45587f4a53b2e4171b7
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-05-05 13:21:16 -0400

    make optional associations on turn
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -3,7 +3,7 @@ class Turn < ApplicationRecord
 
   belongs_to :player
   belongs_to :game
-  belongs_to :roll
-  belongs_to :start_space, class_name: 'Space', primary_key: :id, foreign_key: :start_space_id
-  belongs_to :end_space, class_name: 'Space', primary_key: :id, foreign_key: :end_space_id 
+  belongs_to :roll, optional: true
+  belongs_to :start_space, class_name: 'Space', primary_key: :id, foreign_key: :start_space_id, optional: true
+  belongs_to :end_space, class_name: 'Space', primary_key: :id, foreign_key: :end_space_id, optional: true
 end
