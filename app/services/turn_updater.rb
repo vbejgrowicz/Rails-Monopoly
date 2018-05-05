5b61e44afed9f4d6772d0267a9dd73f6a4059d4e
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-05-05 19:10:26 -0400

    add end space after roll
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/services/turn_updater.rb
+++ app/services/turn_updater.rb
@@ -9,14 +9,17 @@ class TurnUpdater
   end
 
   def run_update!
-    roll if @params[:turn_action] == 'roll'
-    @turn.save! if @turn.changed?
+    ActiveRecord::Base.transaction do
+      roll if @params[:turn_action] == 'roll'
+      @turn.save! if @turn.changed?
+    end
     @turn
   end
 
   def roll
     validate_valid_roll!
     @turn.roll = Roll.create!(GenerateRoll.run(@turn.player_id))
+    @turn.end_space = Space.find_by(position: @turn.start_space.position + @turn.roll.total)
   end
 
   private
