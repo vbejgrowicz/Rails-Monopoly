5540160c91af98b797ce89ebeb66cc2d362b474e
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-05-19 12:43:32 -0400

    add event actions const and get action for event method
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,22 @@ class Event < ApplicationRecord
   validates :name, presence: true
 
   has_many :spaces
+
+  EVENT_ACTIONS = {
+    'Community Chest' => 'draw',
+    'Chance' => 'draw',
+    'Income Tax' => 'pay',
+    'In Jail' => 'none',
+    'Free Parking' => 'none',
+    'Go To Jail' => 'move',
+    'Luxury Tax' => 'pay',
+    'Go' => 'receive',
+  }.freeze
+
+  def self.get_action_for(event)
+    Action.find_by(name: EVENT_ACTIONS[event.name])
+  end
+
   def is_property?
     false
   end
