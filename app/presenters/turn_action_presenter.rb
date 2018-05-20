5c65e5aa7dd9635f13ee6fa3d463804928d47847
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-05-19 22:07:26 -0400

    send id in turn action presenter
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/presenters/turn_action_presenter.rb
+++ app/presenters/turn_action_presenter.rb
@@ -1,6 +1,7 @@
 class TurnActionPresenter < ApplicationPresenter
   def as_json(*)
     {
+      id: @object.id,
       turn_id: @object.turn_id,
       action: @object.action.name,
       completed: @object.completed,
