6fb62eea6a78fb8e17453907d3408a8cb8e586f9
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-05-20 20:48:26 -0400

    update turn action presenter to include game transaction
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/presenters/turn_action_presenter.rb
+++ app/presenters/turn_action_presenter.rb
@@ -1,10 +1,16 @@
 class TurnActionPresenter < ApplicationPresenter
   def as_json(*)
-    {
+    json = {
       id: @object.id,
       turn_id: @object.turn_id,
       action: @object.action.name,
       completed: @object.completed,
     }
+    if @object.game_transaction
+      json[:transaction] = GameTransactionPresenter.new(@object.game_transaction)
+    else
+      json[:transaction] = {}
+    end
+    json
   end
 end
