059b5251447c9f5e6f3addad4997e4c7373b7531
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-06-16 20:42:43 -0400

    create standardCard function for property card
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/javascript/components/Game/Property/PropertyCard.jsx
+++ app/javascript/components/Game/Property/PropertyCard.jsx
@@ -2,59 +2,69 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class PropertyCard extends React.Component {
-  render() {
-    const { item } = this.props;
+  standardCard(item) {
     return (
-      <div className="property-card">
-        <div className="property-data">
-          <div className={`title-group property-color ${item.color}`}>
-            <div className="title-deed">title deed</div>
-            <div className="title-name">{item.title}</div>
+      <div className="property-data">
+        <div className={`title-group property-color ${item.color}`}>
+          <div className="title-deed">title deed</div>
+          <div className="title-name">{item.title}</div>
+        </div>
+        <div className="rent-group">
+          <div className="rent">
+            <div className="rent-label">Rent</div>
+            <div className="rent-price">$26</div>
           </div>
-          <div className="rent-group">
-            <div className="rent">
-              <div className="rent-label">Rent</div>
-              <div className="rent-price">$26</div>
-            </div>
-            <div className="rent">
-              <div className="rent-label">Rent with color set</div>
-              <div className="rent-price">$52</div>
-            </div>
-            <div className="rent">
-              <div className="rent-label">Rent with <i className="fa fa-home house" /></div>
-              <div className="rent-price">$130</div>
-            </div>
-            <div className="rent">
-              <div className="rent-label">Rent with <i className="fa fa-home house" /><i className="fa fa-home house" /></div>
-              <div className="rent-price">$390</div>
-            </div>
-            <div className="rent">
-              <div className="rent-label">Rent with <i className="fa fa-home house" /><i className="fa fa-home house" /><i className="fa fa-home house" /></div>
-              <div className="rent-price">$900</div>
-            </div>
-            <div className="rent">
-              <div className="rent-label">Rent with <i className="fa fa-home house" /><i className="fa fa-home house" /><i className="fa fa-home house" /><i className="fa fa-home house" /></div>
-              <div className="rent-price">$1100</div>
-            </div>
-            <div className="rent">
-              <div className="rent-label">Rent with <i className="fa fa-home hotel" /></div>
-              <div className="rent-price">$1275</div>
-            </div>
+          <div className="rent">
+            <div className="rent-label">Rent with color set</div>
+            <div className="rent-price">$52</div>
           </div>
-          <div className="cost-group">
-            <div className="cost">
-              <div className="cost-label">Houses cost</div>
-              <div className="cost-price">$200 each</div>
-            </div>
-            <div className="cost">
-              <div className="cost-label">Hotels cost</div>
-              <div className="cost-price">$200 each <span>(plus 4 houses)</span></div>
-            </div>
+          <div className="rent">
+            <div className="rent-label">Rent with <i className="fa fa-home house" /></div>
+            <div className="rent-price">$130</div>
+          </div>
+          <div className="rent">
+            <div className="rent-label">Rent with <i className="fa fa-home house" /><i className="fa fa-home house" /></div>
+            <div className="rent-price">$390</div>
+          </div>
+          <div className="rent">
+            <div className="rent-label">Rent with <i className="fa fa-home house" /><i className="fa fa-home house" /><i className="fa fa-home house" /></div>
+            <div className="rent-price">$900</div>
+          </div>
+          <div className="rent">
+            <div className="rent-label">Rent with <i className="fa fa-home house" /><i className="fa fa-home house" /><i className="fa fa-home house" /><i className="fa fa-home house" /></div>
+            <div className="rent-price">$1100</div>
+          </div>
+          <div className="rent">
+            <div className="rent-label">Rent with <i className="fa fa-home hotel" /></div>
+            <div className="rent-price">$1275</div>
+          </div>
+        </div>
+        <div className="cost-group">
+          <div className="cost">
+            <div className="cost-label">Houses cost</div>
+            <div className="cost-price">$200 each</div>
+          </div>
+          <div className="cost">
+            <div className="cost-label">Hotels cost</div>
+            <div className="cost-price">$200 each <span>(plus 4 houses)</span></div>
           </div>
         </div>
       </div>
     );
   }
+
+  render() {
+    const { item } = this.props;
+    return (
+      <div className="property-card">
+        {this.standardCard(item)}
+      </div>
+    );
+  }
 }
+
+PropertyCard.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 
 export default PropertyCard;
