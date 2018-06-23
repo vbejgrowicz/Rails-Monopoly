7e32eee8613e23b10b6dff2ae977b2dd032d8b47
ljr5102 <ljr5102@gmail.com>
2018-06-23 12:39:07 -0400

    include event, property and color set on spaces

--- app/controllers/api/spaces_controller.rb
+++ app/controllers/api/spaces_controller.rb
@@ -1,6 +1,6 @@
 class Api::SpacesController < ApplicationController
   def index
-    @spaces = Space.all
+    @spaces = Space.includes(:event, property: [:color_set]).all
     render json: { spaces: @spaces.map { |space| SpacePresenter.new(space) } }
   end
 end
