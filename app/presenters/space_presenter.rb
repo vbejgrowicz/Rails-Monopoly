2402141ab731ad0387eb1ddede0af940ea38a1f2
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-07-28 15:07:41 -0400

    pull property pricing information out of space presenter
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/presenters/space_presenter.rb
+++ app/presenters/space_presenter.rb
@@ -20,7 +20,6 @@ class SpacePresenter < ApplicationPresenter
       color: nil,
       description: @object.event.description,
       is_event: true,
-      price_data: {},
     }
   end
 
@@ -29,7 +28,6 @@ class SpacePresenter < ApplicationPresenter
       color: @object.property.color_set.color,
       description: nil,
       is_property: true,
-      price_data: property_prices
     }
   end
 
@@ -42,22 +40,5 @@ class SpacePresenter < ApplicationPresenter
 
   def is_railroad?
     @object.tile.name.include?('Railroad') || @object.tile.name == 'Short Line'
-  end
-
-  def property_prices
-    property = @object.property
-    {
-      buy_price: property.buy_price,
-      mortgage_value: property.mortgage_value,
-      mortgage_payoff: property.mortgage_payoff,
-      rent: property.rent,
-      rent_with_set: property.rent_with_set,
-      rent_with_one: property.rent_with_one,
-      rent_with_two: property.rent_with_two,
-      rent_with_three: property.rent_with_three,
-      rent_with_four: property.rent_with_four,
-      rent_with_hotel: property.rent_with_hotel,
-      build_cost: property.build_cost,
-    }
   end
 end
