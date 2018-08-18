764abe281b041a8978e3e6e250c3823144c3e799
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-08-18 15:12:32 -0400

    add ability to toggle game details modal
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/javascript/components/Game/Dock/GameActions.jsx
+++ app/javascript/components/Game/Dock/GameActions.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import GameDetails from './GameDetails';
 
 class GameActions extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      gameDetailsModal: false,
+    };
+    this.toggleGameDetails = this.toggleGameDetails.bind(this);
+  }
+
+  toggleGameDetails() {
+    this.setState({ gameDetailsModal: !this.state.gameDetailsModal });
+  }
+
   render() {
     return (
       <div className="game-actions">
-        <button className="action-btn">Game Details</button>
-        <GameDetails />
+        <button className="action-btn" onClick={this.toggleGameDetails}>Game Details</button>
+        {this.state.gameDetailsModal && <GameDetails onClose={this.toggleGameDetails} />}
       </div>
     );
   }
 }
-
-GameActions.propTypes = {
-};
 
 export default GameActions;
