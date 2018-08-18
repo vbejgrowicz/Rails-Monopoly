fda54d48b2455e319e88c0539ca3a1ca6c9c3435
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-08-18 16:59:51 -0400

    complete the purchase property validations and deduct money on purchase
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/services/purchase_property.rb
+++ app/services/purchase_property.rb
@@ -8,25 +8,30 @@ class PurchaseProperty
   end
 
   def run
-    property = @turn_action.turn.end_space.property
-    deed = property.deed_for(@turn_action.turn.game)
+    @property = @turn_action.turn.end_space.property
+    @deed = @property.deed_for(@turn_action.turn.game)
+    @player = @turn_action.turn.player
     validate_deed_is_not_owned!
     validate_purchase_power!
-    commit_purchase(deed)
+    commit_purchase!
   end
 
   private
 
-  def commit_purchase(deed)
+  def commit_purchase!
     ActiveRecord::Base.transaction do
-      deed.update!(owner_id: @turn_action.turn.player_id)
+      @deed.update!(owner_id: @player.id)
+      @player.update!(money: @player.money - @property.buy_price)
       @turn_action.game_transaction.update!(completed: true)
     end
   end
 
   def validate_deed_is_not_owned!
+    raise 'This property is already owned!' if @deed.owner_id.present?
   end
 
   def validate_purchase_power!
+    return if @player.money >= @property.buy_price
+    raise "You're a bit short on funds!"
   end
 end
