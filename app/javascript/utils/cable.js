b2fe5cf5bf75f7cb5c9da8c591e8141d5ed38372
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-11-17 19:47:15 -0500

    add alternate identifier for cable subscriptions
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/javascript/utils/cable.js
+++ app/javascript/utils/cable.js
@@ -1,9 +1,10 @@
 import ActionCable from 'actioncable';
 
 const connectCable = () => ActionCable.createConsumer('ws://localhost:5000/cable');
-const subscribe = (cable, channel, received) => (
+const subscribe = (cable, channel, received, altIdentifier) => (
   cable.subscriptions.create(channel, {
     received,
+    altIdentifier,
   })
 );
 
