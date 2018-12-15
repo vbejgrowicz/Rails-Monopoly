385caf9287b6b786a23be4a0be80080114e6e280
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-12-14 21:59:41 -0500

    order turn actions by id desc
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/presenters/turn_presenter.rb
+++ app/presenters/turn_presenter.rb
@@ -6,7 +6,7 @@ class TurnPresenter < ApplicationPresenter
       roll: @object.roll ? RollPresenter.new(@object.roll) : {},
       start_space_id: @object.start_space_id,
       end_space_id: @object.end_space_id,
-      actions: @object.turn_actions.map { |turn_action| TurnActionPresenter.new(turn_action) },
+      actions: @object.turn_actions.order(id: :desc).map { |turn_action| TurnActionPresenter.new(turn_action) },
     }
   end
 end
