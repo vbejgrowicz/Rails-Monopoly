6cdb494ff26edd50083e8f827df60765aaded5d5
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2018-12-14 21:59:30 -0500

    generate turn action for card and then update times used on game card
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/services/generate_action_from_card.rb
+++ app/services/generate_action_from_card.rb
@@ -9,6 +9,8 @@ class GenerateActionFromCard
   end
 
   def run
-    TurnAction.create!(turn_id: @turn.id, action_id: card.action_id)
+    TurnAction.create!(turn_id: @turn.id, action_id: @card.action_id)
+    game_card = @card.send(@card.class.joined_assoc).find_by(game_id: @turn.game_id)
+    game_card.update!(times_used: game_card.times_used + 1)
   end
 end
