e97c48f7e70df9832a10a59042c3912d77ad41d0
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2019-05-11 15:34:38 -0400

    create payment service that can process both rent and bank transactions
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/services/payment.rb
+++ app/services/payment.rb
@@ -1,4 +1,4 @@
-class PayRent
+class Payment
   attr_reader :sender, :receiver
 
   def self.run(turn_action)
@@ -22,13 +22,14 @@ class PayRent
 
   def commit_pay!
     ActiveRecord::Base.transaction do
-      @sender.update!(money: @sender.money - @amount)
-      @receiver.update!(money: @receiver.money + @amount)
+      @sender.update!(money: @sender.money - @amount) if @sender
+      @receiver.update!(money: @receiver.money + @amount) if @receiver
       @turn_action.game_transaction.update!(completed: true)
     end
   end
 
   def validate_pay_power!
+    return unless @sender
     return if @sender.money >= @amount
     raise "You're a bit short on funds!"
   end
