fc2ed17f8a477f5556801c074dfb118fc7ab796f
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2019-05-11 15:25:03 -0400

    display formatted title on event card
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/javascript/components/Game/Event/EventCard.jsx
+++ app/javascript/components/Game/Event/EventCard.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const cardTitles = {
+  ChanceCard: 'Chance',
+  CommunityChestCard: 'Community Chest',
+};
+
 class EventCard extends React.Component {
   render() {
     const { type, description } = this.props;
     return (
       <div className="event-card">
-        <div className="event-title">{type}</div>
+        <div className="event-title">{cardTitles[type]}</div>
         <div className="event-body">
           <div className="event-left">{description}</div>
           <div className="event-right" />
