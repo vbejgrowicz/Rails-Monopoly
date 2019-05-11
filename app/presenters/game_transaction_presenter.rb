41f6585939d97a34f885b7c2236eca0af115b7dc
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2019-05-11 14:19:53 -0400

    make property name optional for game transaction presenter
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/presenters/game_transaction_presenter.rb
+++ app/presenters/game_transaction_presenter.rb
@@ -1,13 +1,15 @@
 class GameTransactionPresenter < ApplicationPresenter
   def as_json(*)
-    {
+    json = {
       receiver_id: @object.receiver_id,
       sender_id: @object.sender_id,
       amount: @object.amount,
-      property_name: @object.deed.property.name,
       transaction_type: @object.transaction_type,
+      property_name: nil,
       completed: @object.completed,
       canceled: @object.canceled,
     }
+    json[:property_name] = @object.deed.property.name if @object.deed
+    json
   end
 end
