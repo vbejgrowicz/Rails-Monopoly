46e411070e68cb4cdd2707e6e89cf93051457f45
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2019-05-11 14:18:47 -0400

    add boolean methods for game transaction types
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/models/game_transaction.rb
+++ app/models/game_transaction.rb
@@ -13,4 +13,12 @@ class GameTransaction < ApplicationRecord
   belongs_to :deed, optional: true
   belongs_to :sender, class_name: 'Player', primary_key: :id, foreign_key: :sender_id
   belongs_to :receiver, class_name: 'Player', primary_key: :id, foreign_key: :receiver_id, optional: true
+
+  def rent?
+    transaction_type == 'rent'
+  end
+
+  def bank_transaction?
+    transaction_type == 'bank_transaction'
+  end
 end
