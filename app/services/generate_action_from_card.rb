9c4ee84186d73039a27b7a0330f22d8f4f846c2e
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2019-05-11 14:21:06 -0400

    create game transactions for chance/comm chest card if they are pay or receive actions
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/services/generate_action_from_card.rb
+++ app/services/generate_action_from_card.rb
@@ -9,7 +9,26 @@ class GenerateActionFromCard
   end
 
   def run
-    TurnAction.create!(turn_id: @turn.id, action_id: @card.action_id)
+    turn_action = TurnAction.create!(turn_id: @turn.id, action_id: @card.action_id, card_id: @card.id)
+    if turn_action.pay?
+      GameTransaction.create!(
+        turn_action_id: turn_action.id,
+        transaction_type: 'bank_transaction',
+        sender_id: @turn.player_id,
+        amount: 100, # TODO: amounts should be based off card
+      )
+    elsif turn_action.receive?
+      GameTransaction.create!(
+        turn_action_id: turn_action.id,
+        transaction_type: 'bank_transaction',
+        receiver_id: @turn.player_id,
+        amount: 100, # TODO: amounts should be based off card
+      )
+    elsif turn_action.move?
+      # TODO: movement transaction or something similar?
+    else
+      # TODO: nil action?
+    end
     game_card = @card.send(@card.class.joined_assoc).find_by(game_id: @turn.game_id)
     game_card.update!(times_used: game_card.times_used + 1)
   end
