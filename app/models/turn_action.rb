a0d771d78bb0334bafc145dbc17ad37af06a802c
Vivian Bejgrowicz <vbejgrowicz@gmail.com>
2019-05-18 10:56:19 -0400

    update card association on turn_action to be polymorphic
    Co-Authored-By: Louis Robinson <ljr5102@gmail.com>

--- app/models/turn_action.rb
+++ app/models/turn_action.rb
@@ -4,14 +4,9 @@ class TurnAction < ApplicationRecord
 
   belongs_to :turn
   belongs_to :action
-  belongs_to :chance_card, foreign_key: :card_id, optional: true
-  belongs_to :community_chest_card, foreign_key: :card_id, optional: true
+  belongs_to :card, polymorphic: true, optional: true
 
   has_one :game_transaction
-
-  def card
-    chance_card || community_chest_card
-  end
 
   def buy?
     action.name == 'buy'
